8685377888101ace6c9da631a7ee07b296fc2c2a
Andres Ardila <andresardila33@gmail.com>
2023-08-11 19:54:38 -0500

    update

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ const invitados = [
   "MARIA JOSE",
   "MARÍAJOSÉ",
   "MARÍA JOSÉ",
+  "MARÍA",
   "MÍA",
   "MIA",
   "LUCIANA",
@@ -36,6 +37,7 @@ createApp({
     return {
       invitado: "",
       genero1: false,
+      genero2: false,
       songs: [
         {
           title: "Atrapalos ya",
@@ -48,19 +50,66 @@ createApp({
   },
   methods: {
     findGuest() {
+      if (this.genero1 === true) {
+        this.showMessage1();
+      }
+      if (this.genero2 === true) {
+        this.showMessage2();
+      }
       for (let index = 0; index < invitados.length; index++) {
         if (this.invitado.toUpperCase() === invitados[index]) {
           this.invitado = this.invitado.toUpperCase();
-          this.showMessage();
-          //const element = array[index];
+          const elemento = this.invitado;
+          if (
+            elemento === "MAXIMILIANO" ||
+            elemento === "ALEJANDRO" ||
+            elemento === "VALENTINO" ||
+            elemento === "JOSÉ ANGEL" ||
+            elemento === "JOSÉ" ||
+            elemento === "JOSE ANGEL" ||
+            elemento === "JOSE"
+          ) {
+            this.showMessage1();
+          }
+
+          if (
+            elemento === "ALANA" ||
+            elemento === "ANTONELLA" ||
+            elemento === "MARIANA" ||
+            elemento === "MARIANA" ||
+            elemento === "MARIAJOSE" ||
+            elemento === "MARIA JOSE" ||
+            elemento === "MARÍAJOSÉ" ||
+            elemento === "MARÍA JOSÉ" ||
+            elemento === "MARÍA" ||
+            elemento === "MÍA" ||
+            elemento === "MIA" ||
+            elemento === "LUCIANA" ||
+            elemento === "SOFIA" ||
+            elemento === "SOFÍA" ||
+            elemento === "PROFE SOL" ||
+            elemento === "PROFE" ||
+            elemento === "EILIN" ||
+            elemento === "VALERIA" ||
+            elemento === "EILIN VALERIA"
+          ) {
+            this.showMessage2();
+          }
         }
       }
     },
-    showMessage() {
+    showMessage1() {
       if (this.genero1 === false) {
         this.genero1 = true;
       } else {
         this.genero1 = false;
+      }
+    },
+    showMessage2() {
+      if (this.genero2 === false) {
+        this.genero2 = true;
+      } else {
+        this.genero2 = false;
       }
     },
   },
